6bea3af635f21838c350038db2764c6fae7a2464
elquineas <elquineas@naver.com>
2023-05-11 09:58:53 +0900

    수정

---
+++ src/com/blackjack/exec/Main.java
@@ -0,0 +1,7 @@
+package com.blackjack.exec;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/com/blackjack/models/CardDto.java
@@ -0,0 +1,6 @@
+package com.blackjack.models;
+
+public class CardDto {
+    private String pattern;
+    private String denomination;
+}
