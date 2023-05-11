2854237377ed2860e622af526e0bac3cf8b52903
hee462 <a01080809887@gmail.com>
2023-05-11 16:52:19 +0900

    sart

--- src/com/blackjack/exec/Main.java
+++ src/com/blackjack/exec/Main.java
@@ -3,32 +3,31 @@ package com.blackjack.exec;
 import java.util.Scanner;
 
 import com.blackjack.service.CardService;
+import com.blackjack.service.RuleService;
 
 public class Main {
 	public static void main(String[] args) {
 		CardService cService = new CardService();
+		RuleService rService = new RuleService();
 		Scanner scan = new Scanner(System.in);
-		
-		cService.CreateCard();
-		for(int i = 0; i<2; i++) {
-			cService.addCard("deal");
-			cService.addCard("user");
-		}
-		String text = "";
-		while(true) {
-			cService.printCard();
-			System.out.println("카드를 뽑으시겠습니까? (player/뽑는다)");
-			System.out.print(">>>");
-			text = scan.nextLine();
-			if(text.equals("유저/뽑는다")) {
-				cService.addCard("user");
-			}else if(text.equals("딜러/뽑는다")) {
-				cService.addCard("deal");
-			}else{
-				continue;
-			}
-		}
+
+		rService.stratGame();
+
+//		
+//		String text = "";
+//		while(true) {
+//			cService.printCard();
+//			System.out.println("카드를 뽑으시겠습니까? (player/뽑는다)");
+//			System.out.print(">>>");
+//			text = scan.nextLine();
+//			if(text.equals("유저/뽑는다")) {
+//				cService.addCard("user");
+//			}else if(text.equals("딜러/뽑는다")) {
+//				cService.addCard("deal");
+//			}else{
+//				continue;
+//			}
+//		}
 	}
-	
-	
+
 }
--- src/com/blackjack/service/RuleService.java
+++ src/com/blackjack/service/RuleService.java
@@ -2,4 +2,22 @@ package com.blackjack.service;
 
 public class RuleService {
 
+	CardService cService;
+
+	public RuleService() {
+		cService = new CardService();
+
+	}
+
+	public void stratGame() {
+		cService.CreateCard();
+		
+		for( int i =0 ; i < 2 ; i++) {
+			cService.addCard("deal");
+			cService.addCard("user");
+		}
+		
+		cService.printCard();
+	}
+
 }
